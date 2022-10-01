aec9b39f2caaaedb779be1602736cd8577625f74
Ezequiel Martinez <ezeelmartinez@gmail.com>
2022-09-30 23:22:28 -0300

    3.frontend

--- client/src/Redux/actions/index.js
+++ client/src/Redux/actions/index.js
@@ -19,6 +19,88 @@ const getAllDogs = () => {
 }
 
 
+const getTemperaments = () => {
+    return async function (dispatch) {
+        try {
+            var json = await axios.get('http://localhost3001/temperaments')
+            return dispatch({
+                type: 'GET_TEMPERAMENTS',
+                payload: json.data
+            })
+        }
+        catch(error){
+            throw error;
+        }
+    }
+}
+
+const getDog = (id) => {
+    return async function (dispatch) {
+        try{
+            var json = await axios.get(`http://localhost3001/dogs/${id}`)
+            return dispatch({
+                type: 'GET_DOG',
+                payload: json.data
+            })
+        }
+        catch(error) {
+            throw error;
+        }
+    }
+}
+
+const getDogByName = (name) => {
+    return async function (dispatch) {
+        try {
+            var json = await axios.get(`http://localhost3001/dogs?name=${name}`)
+            return dispatch({
+                type: 'GET_DOG_BY_NAME',
+                payload: json.data
+            })
+        }
+        catch(error) {
+            throw error;
+        }
+    }
+}
+
+const filterByApiDb = (payload) => {
+    return {
+      type: 'FILTER_BY_API_DB',
+      payload,
+    };
+};
+
+const filterByTemperament = (payload) => {
+    return {
+      type: 'FILTER_BY_TEMPERAMENT',
+      payload,
+    };
+};
+
+const orderByName = (payload) => {
+    return {
+      type: 'ORDER_BY_NAME',
+      payload,
+    };
+};
+
+const orderByWeight = (payload) => {
+    return {
+      type: 'ORDER_BY_WEIGHT',
+      payload,
+    };
+};
+
+
+
 export {
     getAllDogs,
+    getTemperaments,
+    getDog,
+    getDogByName,
+    filterByApiDb,
+    filterByTemperament,
+    orderByName,
+    orderByWeight,
 }
--- client/src/Redux/reducer/index.js
+++ client/src/Redux/reducer/index.js
@@ -1,4 +1,24 @@
-function rootReducer(){
+import {
+    GET_ALL_DOGS,
+    GET_DOG,
+    GET_TEMPERAMENTS,
+    GET_DOG_BY_NAME,
+    FILTER_BY_API_DB,
+    FILTER_BY_TEMPERAMENT,
+    ORDER_BY_NAME,
+    ORDER_BY_WEIGHT,
+} from '../actions/index'
+
+
+const initialState = {
+    dog: {},
+    dogs: {},
+    allDogs: [],
+    temperaments: [],
+}
+
+
+const rootReducer = (state = initialState, action) => {
     
 }
 
