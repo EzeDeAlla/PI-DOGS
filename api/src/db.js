bc67ec2f9e3c1c96436b9bdfed19f413b0a82f92
Ezequiel Martinez <ezeelmartinez@gmail.com>
2023-08-05 23:47:13 -0300

    new db in render & deploy back in vercel

--- api/src/db.js
+++ api/src/db.js
@@ -6,7 +6,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/doogs_database`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs_pqv6`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
